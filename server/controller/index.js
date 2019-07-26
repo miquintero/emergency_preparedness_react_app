b7c04e71e01e397259dd11a6eed7c87f79c83704
miquintero <schaeffer.quintero@gmail.com>
2019-07-26 22:40:24 +0200

    feat: express and MongoDB servers running, get requests implemented

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,7 +4,7 @@ const Preparation = require('../model/preparation');
 
 exports.getAlert = async (req, res) => {
   try {
-    const alert = await Alert.findOne({city: req.body}); //make sure req.body represents city
+    const alert = await Alert.findOne({city: req.body}); 
     res.status(200).send(alert);
   } catch (error) {
     console.log('error', error); //eslint-disable-line no-console
@@ -13,46 +13,13 @@ exports.getAlert = async (req, res) => {
   }
 }
 
-exports.seedAlerts = (req, res) => {
-  const alerts = {
-    'city': 'Atlanta',
-    'zipcode': 30305,
-    'title': 'Flood Watch for Buckhead, GA',
-    'severity': 'warning',
-    'time': 1509993360,
-    'expires': 1510036680,
-    'description': '...FLOOD WATCH REMAINS IN EFFECT THROUGH LATE WEDNESDAY NIGHT\n THE HEAVY RAIN WILL PUSH THE\nCHATTAHOOCHE RIVER ABOVE FLOOD STAGE TODAY...AND MAJOR FLOODING IS\nPOSSIBLE.\n*THE FLOODWATCH REMAINS IN EFFECT FOR GWINETT COUNTY FOR THE POSSIBILITY OF\nAREAL FLOODING ASSOCIATED WITH A MAJOR FLOOD.\n',
-    'uri': 'http://alerts.weather.gov/cap/wwacapget.php?x=WA1255E4DB8494.FloodWatch.1255E4DCE35CWA.SEWFFASEW.38e78ec64613478bb70fc6ed9c87f6e6'
-  };
-  const newAlert = new Alert(alerts);
-  newAlert.save();
-  res.send('Database seeded!');
-}
-
 exports.getPreparations = async (req, res) => {
   try {
-    const preparations = await Preparation.findOne({emergency: 'Flood'}); //make sure req.body represents emergency type  
+    const preparations = await Preparation.findOne({emergency: 'Flood'});  
     res.status(200).send(preparations);
   } catch (error) {
     console.log('error', error); //eslint-disable-line no-console
     res.status(500);
     res.send({error: error.errors})
   }
-}
-
-exports.seedPreparations = (req, res) => {
-  const emergencyPreparations = {
-    'emergency': 'Flood',  
-    'list': [
-      'Climb a tree',
-      'Fill your tub with filtered water',
-      'Prepare inflatable devices and canoe',
-      'Elevate electrical components in living and working spaces', 
-      'Waterproof your basement', 
-      'Move furniture, valuables, and pets to a safe place'
-    ]
-  };
-  const newPreparation = new Preparation(emergencyPreparations);
-  newPreparation.save();
-  res.send('Database seeded with preparation!');
 }
--- server/router.js
+++ server/router.js
@@ -4,10 +4,8 @@ const router = express.Router();
 const controller = require('./controller/index');
 
 router.get('/alerts', controller.getAlert);
-router.get('/alerts/seed', controller.seedAlerts);
 
 router.get('/alerts/emergency', controller.getPreparations);
-router.get('/alerts/seeds', controller.seedPreparations);
 
 
 module.exports = router;
