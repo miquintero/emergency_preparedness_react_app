d17303a47920ba1a77286a22eab4d295ccae6208
miquintero <schaeffer.quintero@gmail.com>
2019-10-23 18:31:08 -0300

    style: renamed variables

--- src/components/AlertGeneral/AlertGeneral.js
+++ src/components/AlertGeneral/AlertGeneral.js
@@ -9,28 +9,30 @@ import warning from '../../assets/warning.png';
 const icons = {
   'warning': emergency,
   'alert': alert,
-  'watch': warning
+  'watch': warning,
 }
 
 export class AlertGeneral extends Component {
+
   render () {
+
     const { 
-      listCity, 
       alertCity, 
+      dbCity, 
       emergencyType, 
       severity
     } = this.props;
     return (
       <AlertContainer>
-        { alertCity && alertCity === listCity ?
+        { (dbCity && alertCity === dbCity) ?
           <div>
             <Icon alt="icon of emergency condition" src={icons[severity]}/>
             <AlertText>
-              {`Weather alert for ${(listCity).toUpperCase()}`} 
+              {`Weather alert for ${(dbCity).toUpperCase()}`} 
             </AlertText>
             <AlertButton className='alertbutton'>
-              <StyledLink to={{
-                pathname: `/alerts/${listCity}`,
+              <StyledLink to={{ 
+                pathname: `/alerts/${dbCity}`, 
                 state: emergencyType
               }}>
                 Get more information
@@ -43,15 +45,17 @@ export class AlertGeneral extends Component {
               Good news!<br/>There are currently no weather alerts in your area.
             </AlertText>
             <AlertButton className='alertbutton'>
-              <StyledLink to='/disaster/${disaster}'>
+              <StyledLink to={{
+                pathname:`/disaster/${emergencyType}`
+              }}>
                 In case of future alerts
               </StyledLink>
             </AlertButton>
           </div>
         }
       </AlertContainer>
-      )
-    }
+    )
   }
+};
 
 export default AlertGeneral;
