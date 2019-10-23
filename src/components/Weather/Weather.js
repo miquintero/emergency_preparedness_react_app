3a76524061c6bac806f1d3d770ab15b91e035cde
miquintero <schaeffer.quintero@gmail.com>
2019-10-23 18:23:16 -0300

    refactor: import climate icons

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,36 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import Moment from 'react-moment';
 
 import dataRequests from '../../utils/DataRequests';
 import usePrevious from '../../utils/CompareHook';
 import AlertGeneral from '../AlertGeneral/AlertGeneral';
+import ClimateIcons from './ClimateIcons';
 
-import { Wrapper, WeekDay, Logo, FormContainer, InputForm, UserInput, SubmitButton, City, Region, Temperature, Degrees, Time, ConditionsContainer, Icon, Summary } from './StyleWeather';
-
+import { Wrapper, WeekDay, Logo, FormContainer, InputForm, UserInput, SubmitButton, City, Region, Temperature, Degrees, Time, ConditionsContainer, Summary } from './StyleWeather';
 import LogoImg from '../../assets/listo_transp.png';
-import CLEAR_DAY from '../../assets/climate-icons/clear-day.svg';
-import CLEAR_NIGHT from '../../assets/climate-icons/clear-night.svg';
-import CLOUDY from '../../assets/climate-icons/cloudy.svg';
-import FOG from '../../assets/climate-icons/fog.svg';
-import PARTLY_CLOUDY_DAY from '../../assets/climate-icons/partly-cloudy-day.svg';
-import PARTLY_CLOUDY_NIGHT from '../../assets/climate-icons/partly-cloudy-night.svg';
-import RAIN from '../../assets/climate-icons/rain.svg';
-import SLEET from '../../assets/climate-icons/sleet.svg';
-import SNOW from '../../assets/climate-icons/snow.svg';
-import WIND from '../../assets/climate-icons/wind.svg';
-
-const climateIcons = {
-  'clear-day': CLEAR_DAY, 
-  'clear-night': CLEAR_NIGHT, 
-  'cloudy': CLOUDY, 
-  'fog': FOG, 
-  'partly-cloudy-day': PARTLY_CLOUDY_DAY,
-  'partly-cloudy-night': PARTLY_CLOUDY_NIGHT, 
-  'rain': RAIN, 
-  'sleet': SLEET, 
-  'snow': SNOW, 
-  'wind': WIND,
-};
 
 function Weather () {
 
@@ -41,10 +18,9 @@ function Weather () {
   const [hourlySummary, setHourly] = useState('');
   const [time, setTime] = useState('');
   const [darkskyIcon, setDarksky] = useState('');
-  const [emergencyAlert, setAlert] = useState('');
   const [emergencyType, setEmergencyType] = useState('');
   const [alertCity, setAlertCity] = useState('');
-  const [listCity, setListCity] = useState('');
+  const [dbCity, setDbCity] = useState('');
   const [severity, setSeverity] = useState('');
 
   const getCoords = async () => {
@@ -68,20 +44,18 @@ function Weather () {
         setTime(data.time);
         setHourly(weather.data.hourly.summary);
         setDarksky(data.icon);
-        if (weather.data.alerts) {
-          setAlert(weather.data.alerts);
-        }
       })
       .catch(error => console.log('Error:', error));
   }
-
-  const getCity = async (nameCity) => {
-    await dataRequests  
-      .fetchCity(nameCity)
-      .then(response => {
-        setListCity(response.data.city);
-        setEmergencyType(response.data.type);  
-        setSeverity(response.data.severity);
+    
+    const getAlert = async () => {
+      const city =  nameCity || 'Santiago';
+      await dataRequests
+      .fetchAlert({city})
+      .then(alerts => {
+        setDbCity(alerts.city);
+        setEmergencyType(alerts.type);
+        setSeverity(alerts.severity);
       })
       .catch(error => console.log('Error:', error));
   }
@@ -95,24 +69,19 @@ function Weather () {
     event.preventDefault();
     getCoords();
     getWeather();
-    getCity(nameCity);
     setAlertCity(nameCity);
+    getAlert();
     setNameCity('');
     setDarksky('');
   }
 
   const prevSearch = usePrevious(alertCity);
-  
-  const LoadingIcon = 
-    !darkskyIcon 
-    ? 'Loading icon'
-    : <Icon src={climateIcons[darkskyIcon]} alt="icon of weather conditions"/>;
 
   useEffect(() => {
     getCoords();
     if (coordinates !== { 'lat': 0, 'lng': 0 }) getWeather();
-    getCity(nameCity);
     if (prevSearch !== alertCity) getCoords(alertCity);
+    getAlert();
   }, []);
        
   return (
@@ -134,16 +103,17 @@ function Weather () {
       <City>{nameCity ? nameCity : alertCity}</City> 
       <Region>{hourlySummary}</Region>
       <AlertGeneral 
-        emergencyAlert={emergencyAlert} 
         alertCity={alertCity} 
-        listCity={listCity} 
+        dbCity={dbCity} 
         emergencyType={emergencyType}
         severity={severity}
       /> 
       <Temperature>{temperature}<Degrees>°C</Degrees></Temperature>
       <ConditionsContainer>
         <Summary>The current weather is {currentSummary.toLowerCase()}</Summary>
-        {LoadingIcon}
+        <ClimateIcons 
+          darkskyIcon={darkskyIcon}
+        />
       </ConditionsContainer>    
     </Wrapper>
   );
