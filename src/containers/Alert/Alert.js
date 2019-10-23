2a04cdbbafcc6c8404ae9e42dca38a740c68acbc
miquintero <schaeffer.quintero@gmail.com>
2019-10-23 18:31:58 -0300

    style: added sidebar component

--- src/containers/Alert/Alert.js
+++ src/containers/Alert/Alert.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import AlertBackdrop from '../../components/AlertBackdrop/AlertBackdrop';
+import Sidebar from '../Sidebar/Sidebar';
 
 function Alert () {
 
   return (
     <div>
+      <Sidebar />
       <AlertBackdrop />
     </div>
   )
